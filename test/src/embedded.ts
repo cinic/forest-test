b3842f70c0932f39f5c86a575f8d704f75216d56
Alexander Andreev <cinic.rus@gmail.com>
2020-12-09 19:13:23 +0300

    added router

--- test/src/embedded.ts
+++ test/src/embedded.ts
@@ -1,5 +1,6 @@
 import { styled } from 'foliage'
 import { using, h } from 'forest'
+import { createURLRouter} from 'forest-url-router'
 
 const Block = styled.div`
   display: flex;
@@ -8,13 +9,41 @@ const Header = styled.h1`
   color: magenta;
 `
 
-const Users = () =>
+const Hello = () =>
   Block({
     fn() {
       Header({ text: 'Hello' })
     },
   })
+const Users = () =>
+  Block({
+    fn() {
+      Header({ text: 'Users' })
+    },
+  })
+
+const routes = [
+  {
+    path: '/',
+    exact: true,
+    component: Hello,
+  },
+  {
+    path: '/users',
+    exact: true,
+    component: Users,
+  },
+]
+const { Router, Link } = createURLRouter({ routes })
 
 using(document.getElementById('root') as HTMLElement, () => {
-  Users()
+  h('ul', () => {
+    h('li', () => {
+      Link({ text: 'Home', attr: { href: '/' } })
+    })
+    h('li', () => {
+      Link({ text: 'Users', attr: { href: '/users' } })
+    })
+  })
+  Router()
 })
