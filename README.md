566c10a469dffdc6aef6b62b2970496022272815
Alexander Andreev <cinic.rus@gmail.com>
2020-12-09 18:20:46 +0300

    Added templates

---
+++ README.md
@@ -0,0 +1,14 @@
+# Test lib
+run 
+```sh
+./build.sh
+```
+To test import from foliage dir, go to test dir and run:
+```sh
+yarn start
+```
+
+To test import from node_modules, go to test dir and run:
+```sh
+yarn start:embedded
+```
---
+++ test/src/embedded.ts
@@ -0,0 +1,20 @@
+import { styled } from 'foliage'
+import { using, h } from 'forest'
+
+const Block = styled.div`
+  display: flex;
+`
+const Header = styled.h1`
+  color: magenta;
+`
+
+const Users = () =>
+  Block({
+    fn() {
+      Header({ text: 'Hello' })
+    },
+  })
+
+using(document.getElementById('root') as HTMLElement, () => {
+  Users()
+})
