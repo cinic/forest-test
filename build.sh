a3e3eb2dee1c6f5795e23cda92a327036a8d4d2e
Alexander Andreev <cinic.rus@gmail.com>
2020-12-09 19:15:05 +0300

    changed build, for embedded used foliage from npm

--- build.sh
+++ build.sh
@@ -3,8 +3,3 @@ git submodule init
 git submodule update
 cd foliage && yarn && yarn build
 cd ../test && yarn
-cd ..
-mkdir ./test/node_modules/foliage
-cp -r ./foliage/dist ./test/node_modules/foliage/dist
-cp ./foliage/package.json ./test/node_modules/foliage/package.json
-cp -r ./foliage/node_modules/stylis ./test/node_modules/stylis
